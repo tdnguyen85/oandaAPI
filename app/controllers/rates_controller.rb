2782012e1f93c93507e8d66bc7b3b4c5c7c7fa3e
Tan Nguyen <tdnguyen85@gmail.com>
2013-05-17 09:36:23 -0700

    Format to .json

--- app/controllers/rates_controller.rb
+++ app/controllers/rates_controller.rb
@@ -3,11 +3,15 @@ class RatesController < ApplicationController
 
   def statusboard
     @chosen_rate = Rate.new("EUR_USD")
-    render :json => @chosen_rate.formatted_single
+    respond_to do |format|
+      format.json { render :json => @chosen_rate.formatted_single }
+    end
   end
 
   def statusboardcandle
     @chosen_rate_candle = Rate.new("EUR_USD")
-    render :json => @chosen_rate_candle.formatted_candle
+    respond_to do |format|
+      format.json { render :json => @chosen_rate_candle.formatted_candle }
+    end
   end
 end
