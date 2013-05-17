6eb239eacbc59fca44f779943a0081f2edb6cb92
Tan Nguyen <tdnguyen85@gmail.com>
2013-05-17 10:14:29 -0700

    fixed json field from int to string

--- spec/controllers/rate_controller_spec.rb
+++ spec/controllers/rate_controller_spec.rb
@@ -8,12 +8,24 @@ describe RatesController do
       expect(chosen_rate).to be_true
     end
 
+    it 'respond to json call' do
+      get :statusboard
+      expect(response).to be_success
+    end
+
+
   end
 
   describe :statusboardcandle do
     it 'should have a chosen rate' do
       chosen_rate = "EUR_USD"
       expect(chosen_rate).to be_true
+    end
+
+
+    it 'respond to json call' do
+      get :statusboardcandle
+      expect(response).to be_success
     end
   end
 end
